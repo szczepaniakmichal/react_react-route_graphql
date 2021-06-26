f5a8e0b530f8708866d4fd6d34e2ed1a24fecded
szczepaniakmichal <mssk87@gmail.com>
2021-06-27 00:51:01 +0200

    added component ContinentsList / get and render all continents

--- src/pages/ContinentsList.js
+++ src/pages/ContinentsList.js
@@ -1,7 +1,55 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import {
+    ApolloClient,
+    InMemoryCache,
+    ApolloProvider,
+    gql
+} from "@apollo/client";
+import { Link } from "react-router-dom";
 
-export const ContinentsList = () => {
+export const ContinentsList = ({match}) => {
+
+    const [continents, setContinents] = useState([])
+
+    useEffect(() => {
+        const getContinents = new ApolloClient({
+            uri: 'https://countries.trevorblades.com',
+            cache: new InMemoryCache()
+        });
+        // GET CONTINENTS
+        getContinents
+            .query({
+                query: gql`
+                  query data {
+                    continents {
+                      name
+                      code
+                    }
+                  }
+                `
+            })
+            .then(result => {
+                setContinents(result.data.continents)
+            });
+    }, [])
+
+    const continentsJsx = continents.map(continent => (
+        <li key={continent.name}>
+            <Link to={`/continents/${continent.code}`} >
+                <p>Continent name: {continent.name}</p>
+                <p>Continent code: {continent.code}</p>
+            </Link>
+        </li>
+    ))
+
     return (
-        <p>continents list</p>
+        <ApolloProvider client={continents}>
+            <div>
+                <p>ContinentsList</p>
+                <ul>
+                    {continentsJsx}
+                </ul>
+            </div>
+        </ApolloProvider>
     )
 }
