3c66d8cc7fc4d56d15e4e590201ffd6abf23df40
szczepaniakmichal <mssk87@gmail.com>
2021-06-27 15:32:56 +0200

    added go to home page when page not found

--- src/pages/Error.js
+++ src/pages/Error.js
@@ -1,5 +1,12 @@
 import React from 'react';
+import { Link } from "react-router-dom";
 
 export const Error = () => (
-    <h2>Page not found</h2>
+    <>
+        <h2 className="p-2 bg-yellow-200 text-center">Page not found</h2>
+        <Link to='/'
+              className="block mb-5 p-2 text-center bg-yellow-300 hover:bg-yellow-400 transition duration-300"
+        >Go back to home page</Link>
+    </>
+
 )
