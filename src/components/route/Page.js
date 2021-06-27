2b8a3ec56b5b9fa23d608f9b501954c034f1c818
szczepaniakmichal <mssk87@gmail.com>
2021-06-27 14:28:23 +0200

    added component Error - info if page not found

--- src/components/route/Page.js
+++ src/components/route/Page.js
@@ -4,11 +4,13 @@ import { Route, Switch } from 'react-router-dom';
 import { HomePage } from "../../pages/HomePage";
 import { ContinentsList } from "../../pages/ContinentsList";
 import { Continent } from "../../pages/Continent";
+import { Error } from "../../pages/Error";
 
 export const Page = () => (
     <Switch>
         <Route path='/' exact component={HomePage} />
         <Route path='/continents' exact component={ContinentsList} />
         <Route path='/continents/:code' component={Continent}/>
+        <Route component={Error} />
     </Switch>
 );
---
+++ src/pages/Error.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export const Error = () => (
+    <h2>Page not found</h2>
+)
