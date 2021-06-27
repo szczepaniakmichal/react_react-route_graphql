bcfe9bda95f760a9f282944877067153a0ef1311
szczepaniakmichal <mssk87@gmail.com>
2021-06-27 14:31:45 +0200

    added Links for go to higher page

--- src/pages/Continent.js
+++ src/pages/Continent.js
@@ -6,6 +6,7 @@ import {
 } from "@apollo/client";
 
 import { CurrentContinent } from "./CurrentContinent";
+import { Link } from "react-router-dom";
 
 export const Continent = ({match}) => {
 
@@ -39,13 +40,24 @@ export const Continent = ({match}) => {
             });
     }, []);
 
-    const countriesJsx = countries.map(countrie => (
-        <li key={countrie.name}>
-            <p>countrie name: {countrie.name}</p>
-            <p>countrie emoji: {countrie.emoji}</p>
-            <p>countrie languages: {countrie.languages[0].name}</p>
-        </li>
-    ));
+    const countriesJsx = countries.length > 0 ? countries.map(countrie => {
+        const {name, emoji, languages} = countrie;
+        return (
+            <li key={countrie.name}
+                className="p-2 border-2 border-gray-200 hover:bg-yellow-200 transition duration-300"
+            >
+                <p>countrie name: <span className="font-bold">{name}</span></p>
+                <p>countrie emoji: <span className="font-bold">{emoji}</span></p>
+                <p>countrie languages: <span className="font-bold">{languages && languages[0] && languages[0].name}</span></p>
+            </li>
+        )
+    }) : null;
+
+        const linkJsx = countries.length > 12 ? (
+            <Link to='/continents/'
+                  className="block mb-5 p-2 text-center bg-yellow-300 hover:bg-yellow-400 transition duration-300"
+            >Go back to continents list</Link>
+        ) : null;
 
     return (
         <div>
@@ -53,6 +65,7 @@ export const Continent = ({match}) => {
             <ul>
                 {countriesJsx}
             </ul>
+            {linkJsx}
         </div>
     );
 };
--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,7 +1,10 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 export const HomePage = () => {
     return (
-        <a href="/continents">link do listy kontynentów</a>
+        <Link to='/continents/'
+              className="block p-2 bg-yellow-300 hover:bg-yellow-400 transition duration-300"
+        >Go to continent list</Link>
     )
 }
